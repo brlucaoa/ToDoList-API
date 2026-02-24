021ec6fe589831d6dfad3148747c3008d3775691
Lucas Gomes <204907@upf.br>
2026-02-24 07:24:00 -0300

    create repositories

--- src/main/java/com/lucasgomes/todolist/models/Task.java
+++ src/main/java/com/lucasgomes/todolist/models/Task.java
@@ -22,7 +22,7 @@ public class Task {
     @JoinColumn( name = "user_id", nullable = false, updatable = false)
     private User user;
 
-    @Column(name = "desciption", length = 255, nullable = false)
+    @Column(name = "description", length = 255, nullable = false)
     @NotBlank
     @Size(min = 1, max = 255)
     private String description;
