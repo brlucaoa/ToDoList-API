215b40902ef726ee684ce4604bfaa62082d70a01
Lucas Gomes <204907@upf.br>
2026-03-05 21:35:19 -0300

    Create Task Service

--- src/main/java/com/lucasgomes/todolist/models/Task.java
+++ src/main/java/com/lucasgomes/todolist/models/Task.java
@@ -41,7 +41,7 @@ public class Task {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
---
+++ src/main/java/com/lucasgomes/todolist/services/TaskService.java
@@ -0,0 +1,52 @@
+package com.lucasgomes.todolist.services;
+
+import com.lucasgomes.todolist.models.Task;
+import com.lucasgomes.todolist.models.User;
+import com.lucasgomes.todolist.repositories.TaskRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserService userService;
+
+    public Task findById(Long id) {
+        Optional<Task> task = this.taskRepository.findById(id);
+        return task.orElseThrow(() -> new RuntimeException("Tarefa não encontrada" + id + ", Tipo: " + Task.class.getName()
+        ));
+    }
+
+    @Transactional
+    public Task create(Task obj) {
+        User user = this.userService.findById(obj.getUser().getId());
+        obj.setId(null);
+        obj.setUser(user);
+        obj = this.taskRepository.save(obj);
+        return obj;
+    }
+
+    @Transactional
+    public Task update(Task obj) {
+        Task newObj = this.findById(obj.getId());
+        newObj.setDescription(obj.getDescription());
+        return this.taskRepository.save(newObj);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        findById(id);
+        try{
+                this.taskRepository.deleteById(id);
+                }catch(Exception e){
+            throw new RuntimeException("Não é possível deletar pois há entidades relacionadas");
+        }
+    }
+
+}
